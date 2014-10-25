b3e4dd9706ff6a1ad2ed54bdd9df45a9c2a4e529
irispineda <irispineda85@gmail.com>
2014-10-25 00:54:48 -0600

    Gestion de alumnos

--- PalasAtenea/ScriptsCambiosEnLaBase/Iris19102014.sql
+++ PalasAtenea/ScriptsCambiosEnLaBase/Iris19102014.sql
@@ -1,9 +1,11 @@
 DROP TABLE IF EXISTS `palasatenea`.`alumno`;
 CREATE TABLE  `palasatenea`.`alumno` (
   `ident` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `nombre1` varchar(50) NOT NULL,
-  `nombre2` varchar(50) NOT NULL,
-  `apellido1` varchar(50) NOT NULL,
+  `codigo` varchar(8) DEFAULT NULL,
+  `estado` smallint(5) unsigned DEFAULT NULL,
+  `nombre1` varchar(50) DEFAULT NULL,
+  `nombre2` varchar(50) DEFAULT NULL,
+  `apellido1` varchar(50) DEFAULT NULL,
   `apellido2` varchar(50) DEFAULT NULL,
   `genero` varchar(1) DEFAULT NULL,
   `fnacimiento` datetime DEFAULT NULL,
@@ -17,6 +19,14 @@ CREATE TABLE  `palasatenea`.`alumno` (
   `aprendizaje` varchar(500) DEFAULT NULL,
   `escuelaprevia` varchar(250) DEFAULT NULL,
   `ultgrado` int(10) unsigned DEFAULT NULL,
-  `codigo` varchar(8) DEFAULT NULL,
   PRIMARY KEY (`ident`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `palasatenea`.`codalum`;
+CREATE TABLE  `palasatenea`.`codalum` (
+  `ident` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `codigo` varchar(2) DEFAULT NULL,
+  `anio` int(10) unsigned DEFAULT NULL,
+  `correlativo` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`ident`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
